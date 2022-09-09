696c64cdf10a93a984e26ce6072a1899c39cd0d4
ammyee <okoyeamarachukwu13@gmail.com>
2022-09-08 22:45:14 -0700

    See puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - print "Programming is like building a multilingual puzzle"
+*
+* Return: 0 if exited properly, non-zer otherwise
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
