ff59ba22fe6166180d731c6c3a0cefefea57f94a
ammyee <okoyeamarachukwu13@gmail.com>
2022-09-22 11:17:13 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,15 @@
-#include "main"
+#include "main.h"
 
 /**
-* rot13 -encodes a string using rot13
-* @s: input string
+* rot13 - encodes a string using rot13
+* @s: input string.
 * Return: the pointer to dest.
 */
 char *rot13(char *s)
 {
 	int count = 0, i;
-	char alphabet[] = "abcdefghijklmnopqrstuvwqyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZ";
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	while (*(s + count) != '\0')
 	{
